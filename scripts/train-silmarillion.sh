9066c8e9fa161d8b2a0f57cdc12c52752136d443
fredbwang <wbrzju@outlook.com>
2017-05-12 16:40:28 -0500

    Update train-silmarillion.sh

--- scripts/train-silmarillion.sh
+++ scripts/train-silmarillion.sh
@@ -1,7 +1,7 @@
-python train.py 
---data_file=data/The_silmarillion-utf-8.txt \
---encoding="utf_8" \
---batch_size=100 \
---output_dir=trained_silmarillion/model
+python train.py \
+       --data_file=data/The_silmarillion-utf-8.txt \
+       --encoding="utf_8" \
+       --batch_size=100 \
+       --output_dir=trained_silmarillion/model
 
 tensorboard --logdir=silmarillion/tensorboard_log/ --port=6006
