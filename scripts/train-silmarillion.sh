efbb4dcb3ee25dfc5645567381d01376bc770126
fredbwang <wbrzju@outlook.com>
2017-05-12 11:07:15 -0500

    Add files via upload

---
+++ scripts/train-silmarillion.sh
@@ -0,0 +1,7 @@
+python train.py 
+--data_file=data/The_silmarillion-utf-8.txt \
+--encoding="utf_8" \
+--batch_size=100 \
+--output_dir=trained_silmarillion/model
+
+tensorboard --logdir=silmarillion/tensorboard_log/ --port=6006
